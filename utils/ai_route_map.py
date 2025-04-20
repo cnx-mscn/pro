f92141ee2b5cf4c73c20df1ee81dbc2914ca2d32
cnx-mscn <cmusacan@outlook.com>
2025-04-20 16:44:36 +0300

    Update ai_route_map.py

--- utils/ai_route_map.py
+++ utils/ai_route_map.py
@@ -1,7 +1,7 @@
 
 import openai
 
-openai.api_key = "YOUR_OPENAI_API_KEY"
+openai.api_key = "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"
 
 def rota_olustur(gorevler):
     sehirler = [g["sehir"] for g in gorevler if g["durum"] == "beklemede"]
