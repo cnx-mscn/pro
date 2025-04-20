552f02d44e5d3ebb108ba007c2d661967a46ca8f
cnx-mscn <cmusacan@outlook.com>
2025-04-20 17:45:47 +0300

    Update firebase_config.py

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials, firestore
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase/firebase_service_account.json")
+        cred = credentials.Certificate("firebase/montaj-7ad7d-firebase-adminsdk-fbsvc-1ae50963aa.json")
         firebase_admin.initialize_app(cred)
     return firestore.client()
