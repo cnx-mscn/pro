70d09d00518d9d9d4dc0918594080b8e7fdc9814
cnx-mscn <cmusacan@outlook.com>
2025-04-20 19:52:50 +0300

    Update firebase_config.py

--- firebase/firebase_config.py
+++ firebase/firebase_config.py
@@ -1,12 +1,8 @@
-import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+        cred = credentials.Certificate("firebase/montaj-7ad7d-firebase-adminsdk-fbsvc-1ae50963aa.json")
         firebase_admin.initialize_app(cred)
     return firestore.client()
